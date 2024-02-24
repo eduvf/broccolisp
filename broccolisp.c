0a7bd04355a312782b32a5f401833f15b4dbd142
eduvf <155389249+eduvf@users.noreply.github.com>
2024-02-24 18:04:38 +0100

    rudimentary input

--- broccolisp.c
+++ broccolisp.c
@@ -1,12 +1,34 @@
 #include "raylib.h"
+#include <stdbool.h>
+
+char input[256] = "\0";
+int input_len = 0;
+
+void check_input() {
+  int key = GetCharPressed();
+
+  if (key > 0) {
+    if ((key >= 32) && (key <= 125) && (input_len < 255)) {
+      input[input_len++] = (char)key;
+      input[input_len] = '\0';
+    }
+  }
+}
 
 int main() {
-  InitWindow(500, 500, "broccolisp");
+  const int SCREEN_WIDTH = 630;
+  const int SCREEN_HEIGHT = 360;
+
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "broccolisp");
+
+  SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
+    check_input();
+
     BeginDrawing();
 
-    DrawText("Hi!", 200, 200, 20, RED);
+    DrawText(input, 20, 20, 20, WHITE);
 
     EndDrawing();
   }
