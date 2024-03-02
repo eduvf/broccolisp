714a466370609a2ed580fbdde94eb01a8ee34bd5
eduvf <155389249+eduvf@users.noreply.github.com>
2024-03-02 18:11:03 +0100

    add function prototypes

--- lisp.h
+++ lisp.h
@@ -29,4 +29,12 @@ static const atom_type nil = {};
 #define head_macro(p) ((p).value.pair->head)
 #define tail_macro(p) ((p).value.pair->tail)
 
+atom_type bl_pair(atom_type head, atom_type tail);
+atom_type bl_int(long x);
+atom_type bl_sym(const char *s);
+
+int bl_lex(const char *s, const char **from, const char **to);
+
+void bl_print(atom_type atom);
+
 #endif
