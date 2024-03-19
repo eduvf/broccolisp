ae66e4ca48874f96ede51a4ed2b9a55ec43b78ec
eduvf <155389249+eduvf@users.noreply.github.com>
2024-03-19 22:59:21 +0100

    complete car & cdr implementation

--- func.c
+++ func.c
@@ -5,6 +5,19 @@ int fn_lisp_car(type_atom args, type_atom *result) {
   if (args.type == NIL || tail(args).type != NIL) {
     return ARGUMENT_NUMBER_ERROR;
   }
+
+  if (head(args).type == NIL) {
+    // is nil (nothing to return)
+    *result = nil;
+  } else if (head(args).type != PAIR) {
+    // is not a pair (cannot get the CAR of an atom)
+    return TYPE_ERROR;
+  } else {
+    // is a pair, get the CAR (== HEAD)
+    *result = head(head(args));
+  }
+
+  return NO_ERROR;
 }
 
 int fn_lisp_cdr(type_atom args, type_atom *result) {
@@ -12,6 +25,19 @@ int fn_lisp_cdr(type_atom args, type_atom *result) {
   if (args.type == NIL || tail(args).type != NIL) {
     return ARGUMENT_NUMBER_ERROR;
   }
+
+  if (head(args).type == NIL) {
+    // is nil (nothing to return)
+    *result = nil;
+  } else if (head(args).type != PAIR) {
+    // is not a pair (cannot get the CAR of an atom)
+    return TYPE_ERROR;
+  } else {
+    // is a pair, get the CDR (== TAIL)
+    *result = tail(head(args));
+  }
+
+  return NO_ERROR;
 }
 
 int fn_lisp_cons(type_atom args, type_atom *result) {
